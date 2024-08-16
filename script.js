74b259dea4ca16db01aa56faef56c48df589ccde
Pradeep Rekapalli <rekkapallipradeepg@gmail.com>
2024-08-16 14:45:47 -0400

    added expereince section

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+
+// const content = document.getElementsByClassName('article-container');
+// let skills = ["HTML","CSS","Angular","Reactjs","JavaScript"]
+// for (let i = 0; i < skills.length(); i++) {
+//     const article = `
+//         <article>
+//             <img src="./Assests/Checkmark2.png" alt="Checkmark" class="icon">
+//             <div>
+//                 <h3>${skills[i]}</h3>
+//                 <p>Experienced</p>
+//             </div>
+//         </article>
+//     `;
+//     content.innerHTML += article;
+// }
